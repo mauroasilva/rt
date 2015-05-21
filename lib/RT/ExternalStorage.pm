68e81bc859932007f574f12af7132bdb7954fa0e
Shawn M Moore <shawn@bestpractical.com>
2015-05-21 20:09:02 +0000

    Remove boilerplate CPAN doc from RT::ExternalStorage
        RT's own packages don't typically include author, copyright, etc.
        in POD; it's in the BPS TAGGED BLOCK

--- lib/RT/ExternalStorage.pm
+++ lib/RT/ExternalStorage.pm
@@ -254,28 +254,4 @@ sub StoreExternally {
     }
 }
 
-=head1 AUTHOR
-
-Best Practical Solutions, LLC E<lt>modules@bestpractical.comE<gt>
-
-=head1 BUGS
-
-All bugs should be reported via email to
-
-    L<bug-RT-Extension-ExternalStorage@rt.cpan.org|mailto:bug-RT-Extension-ExternalStorage@rt.cpan.org>
-
-or via the web at
-
-    L<rt.cpan.org|http://rt.cpan.org/Public/Dist/Display.html?Name=RT-Extension-ExternalStorage>.
-
-=head1 COPYRIGHT
-
-This extension is Copyright (C) 2009-2015 Best Practical Solutions, LLC.
-
-This is free software, licensed under:
-
-  The GNU General Public License, Version 2, June 1991
-
-=cut
-
 1;
